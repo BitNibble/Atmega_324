c99638fcf4d45159a73de2d3848ba79b0c7d6b93
BitNibble <sergio.salazar.santos@gmail.com>
2023-04-26 17:02:33 +0100

    Atmega 324

---
+++ Proj_atmega324_1/atmega324/Debug/makedep.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Libraria\atmega324analog.c
+
+Libraria\atmega324preamble.c
+
+Libraria\atmega324timer.c
+
+Libraria\atmegaeeprom.c
+
+Libraria\function.c
+
+Libraria\keypad.c
+
+Libraria\lcd.c
+
+Libraria\lfsm.c
+
+Libraria\rotenc.c
+
+Libraria\uart.c
+
+main.c
+
